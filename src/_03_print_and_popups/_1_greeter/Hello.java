02088200b6724cc15ce00def9bd1dd7280269c58
league <league@imac-22.attlocal.net>
2025-04-26 11:32:48 -0700

    YES

---
+++ src/_03_print_and_popups/_1_greeter/Hello.java
@@ -0,0 +1,12 @@
+package _03_print_and_popups._1_greeter;
+
+import javax.swing.JOptionPane;
+
+public class Hello {
+public static void main(String[] args) {
+	System.out.println("hello world");
+	String input = JOptionPane.showInputDialog("What is your name?");
+	System.out.println(input);
+	JOptionPane.showMessageDialog(null,"Hello " +input + "! Hope you're well!");
+}
+}
--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,9 +8,14 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String somting = JOptionPane.showInputDialog("Hello, do you know how to write code??????????????????????");
 		// 2. If they say "yes", tell them they will rule the world.
-
+if(somting.equals("yes")) {
+	JOptionPane.showMessageDialog(null, "noice, you will rule the world!!!");
+}
+else {
+	JOptionPane.showMessageDialog(null, "ur trash!!!!!!!");
+}
 		// 3. Otherwise, tell them to sign up for classes at The League.
 
 	}
